83acaf9c499281c00a3a36ef281144f6a99f27bd
purmirl <gmlrnjs1994@gmail.com>
2020-11-17 22:22:24 +0900

    icmp studying

--- experiment/src/package/main.py
+++ experiment/src/package/main.py
@@ -1,4 +1,15 @@
 """
+ PeTrA's Scapy Research Laboratory 2020 ~
+ Experiment
 
+ Note
 
+  01. (pip install scapy) or (pip install --pre scapy) in windows powershell or linux terminal
 """
+
+from scapy.layers.inet import IP, ICMP
+from scapy.sendrecv import sr1
+
+# icmp
+r = sr1(IP(dst="8.8.8.8")/ICMP()/"HelloWorld")
+r.show()
