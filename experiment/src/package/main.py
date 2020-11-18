d6c91028c51cb1eb503d09458838ac7460d0f3fc
purmirl <gmlrnjs1994@gmail.com>
2020-11-18 23:56:01 +0900

    tcp studying

--- experiment/src/package/main.py
+++ experiment/src/package/main.py
@@ -7,13 +7,13 @@
   01. (pip install scapy) or (pip install --pre scapy) in windows powershell or linux terminal
 """
 
-from scapy.layers.inet import IP, ICMP
+from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import send
 
-# ICMP
-destination_ip = "8.8.8.8"
-data = "hello world"
-icmp_packet = IP(dst = destination_ip) / ICMP() / data
-icmp_packet.show()
+# TCP
+destination_ip = ""
+destination_port = ""
+tcp_flags = "" # U, A, P, R, S, F
+tcp_packet = IP(dst = destination_ip) / TCP(dport = destination_port, flags = tcp_flags)
 
-send(icmp_packet)
+send(tcp_packet)
