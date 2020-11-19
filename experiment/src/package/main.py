0df8a022759ecc27084ed929c906a565af25619a
purmirl <gmlrnjs1994@gmail.com>
2020-11-19 19:36:54 +0900

    tcp studying

--- experiment/src/package/main.py
+++ experiment/src/package/main.py
@@ -11,9 +11,9 @@ from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import send
 
 # TCP
-destination_ip = ""
-destination_port = ""
-tcp_flags = "" # U, A, P, R, S, F
+destination_ip = "192.168.35.97"
+destination_port = 23
+tcp_flags = "S" # U, A, P, R, S, F
 tcp_packet = IP(dst = destination_ip) / TCP(dport = destination_port, flags = tcp_flags)
 
 send(tcp_packet)
