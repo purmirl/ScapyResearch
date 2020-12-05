c88a3cf6ea0e62c8d25328b758756923992e358a
purmirl <gmlrnjs1994@gmail.com>
2020-12-05 23:15:30 +0900

    arp studying

---
+++ code/arp.py
@@ -0,0 +1,8 @@
+"""
+ PeTrA's Scapy Research Laboratory 2020 ~
+ Copyrights 2020 PeTrA. All rights reserved
+
+ ARP Example
+
+ ARP : Address Resolution Protocol
+"""
--- experiment/src/package/main.py
+++ experiment/src/package/main.py
@@ -7,20 +7,26 @@
   01. (pip install scapy) or (pip install --pre scapy) in windows powershell or linux terminal
 """
 
-from scapy.layers.inet import IP, UDP
-from scapy.packet import Raw
-
-# TCP
-from scapy.sendrecv import send
-from scapy.volatile import RandShort
 
 
-destination_ip = "192.168.35.102"
-source_port = RandShort()
-destination_port = 22
+# ARP
+from scapy.layers.l2 import Ether, ARP
 
-udp_packet = IP(dst = destination_ip) / UDP(dport = destination_port) / Raw(load = "abc")
-send(udp_packet)
+client_mac = ""
+source_protocol_address = ""
+destination_protocol_address = ""
+arp_packet = Ether(dst = client_mac) / ARP(op = "who-has", psrc = source_protocol_address, pdst = destination_protocol_address)
+
+
+"""
+destination_port = 135
+tcp_flags = "S" # U, A, P, R, S, F
+
+tcp_packet = IP(dst = destination_ip) / TCP(sport = source_port, dport = destination_port, flags = tcp_flags)
+tcp_packet.show()
+
+send(tcp_packet)
+"""
 """
 from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import send
